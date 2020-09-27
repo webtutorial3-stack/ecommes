2d4fade5872b3a14f765dae97928614e66649fb9
unknown <you@example.com>
2020-09-27 20:56:08 +0100

    initial commit

--- ecommes/settings.py
+++ ecommes/settings.py
@@ -86,30 +86,30 @@ WSGI_APPLICATION = 'ecommes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'ecom',
+      'USER': 'postgres',
+      'PASSWORD': '12345',
+      'HOST': 'localhost',
+      'PORT': '5432',
+  }
 }
 
 
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#       'NAME': 'ecom',
-#       'USER': 'postgres',
-#       'PASSWORD': '12345',
-#       'HOST': 'localhost',
-#       'PORT': '5432',
-#   }
-#}
-#
-#
-#import dj_database_url
-#
-#db_from_env =dj_database_url.config()
-#DATABASES['default'].update(db_from_env)
+import dj_database_url
+
+db_from_env =dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -143,6 +143,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 ROBOTS_USE_SCHEME_IN_HOST = True
 
