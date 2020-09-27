3f878749d6091470d1bd9849e980016e90278836
unknown <you@example.com>
2020-09-27 01:14:36 +0100

    Update production for db

--- ecommes/settings.py
+++ ecommes/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'ecommes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
